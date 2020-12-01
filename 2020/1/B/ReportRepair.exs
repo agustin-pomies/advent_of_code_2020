34bdfcfd732eb7dd3fa5f74746ee47cbc09a0341
Agustin Pomies <agustin@eagerworks.com>
2020-12-01 16:58:45 -0300

    Level 1 - Stage B

---
+++ 2020/1/B/ReportRepair.exs
@@ -0,0 +1,49 @@
+defmodule ReportRepair do
+  def fix_report(file_path) do
+    expenses = import_expense_report(file_path)
+    possible_trios = cartesian_product(expenses, expenses, expenses)
+    result = check_sum(possible_trios)
+
+    case result do
+      {:ok , {x, y, z}} -> {{x, y, z}, x * y * z}
+      {:error, reason} -> IO.puts(reason)
+    end
+  end
+  defp import_expense_report(file_path) do
+    report_data = 
+      case File.read(file_path) do
+        {:ok, file}      -> String.split(file, "\n")
+        {:error, reason} -> IO.puts(reason)
+      end
+
+    expenses = Enum.map(report_data, &(convert_to_integer(&1)))
+
+    expenses
+  end
+
+  defp convert_to_integer(my_string) do
+    result = Integer.parse(my_string)
+
+    case result do
+      {number, _} -> number
+      :error -> "It didn't work"
+    end
+  end
+
+  defp cartesian_product(a, b, c) do
+    for x <- a, y <- b, z <- c, do: {x, y, z}
+  end
+
+  defp check_sum([head | tail]) do
+    case head do
+      {x, y, z} when x + y + z == 2020 ->
+        {:ok, head}
+      _ ->
+        check_sum(tail)
+    end
+  end
+
+  defp check_sum([]) do
+    {:error, "No matching entries were found"}
+  end
+end
