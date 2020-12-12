e11e4b34e9d7c6e55dcf7b259dffb6fe625dea96
Agustin Pomies <agustin@eagerworks.com>
2020-12-11 23:42:23 -0300

    Level 7 - Stage A

---
+++ lib/solvers/7_handy_haversacks.ex
@@ -0,0 +1,67 @@
+defmodule HandyHaversacks do
+  def get_data do
+    IOModule.get_input("7") |> parse_input()
+  end
+
+  def part_one do
+    get_data()
+    |> build_graph()
+    |> Graph.reaching_neighbors([color_bag()])
+    |> length()
+  end
+
+  def part_two do
+    0
+  end
+
+  def parse_input(sentences) do
+    sentences
+    |> Enum.map(&(parse_sentence(&1)))
+    |> Enum.reduce(fn elem, acc -> Map.merge(acc, elem) end)
+  end
+
+  def parse_sentence(sentence) do
+    [bag_color | [parsed_sentence]] = 
+      String.replace(sentence, ".", "")
+      |> String.split(" bags contain ")
+
+    contained_bags =
+      parsed_sentence
+      |> String.split(", ")
+      |> Enum.map(&(parse_contained_bag(&1)))
+    
+    %{bag_color => contained_bags}
+  end
+
+  def parse_contained_bag(bag_contained_sentence) do
+    regex = ~r/^([[:digit:]]+) ([[:alpha:]]+ [[:alpha:]]+)/
+
+    case Regex.scan(regex, bag_contained_sentence) do
+      [] -> %{}
+      [[_full_match, weight, color_contained]] -> {color_contained, weight: Helper.to_integer(weight)}
+    end
+  end
+
+  def parse_edges(_color_bag, [%{}]) do
+    []
+  end
+
+  def parse_edges(color_bag, contained) do
+    result = Enum.map(
+      contained,
+      &((fn {color_contained, weight: b} -> {color_bag, color_contained, weight: b} end).(&1))
+    )
+
+    result
+  end
+  
+  def build_graph(mapping) do
+    g = Graph.new
+    g = Graph.add_vertices(g, Map.keys(mapping))
+    Enum.reduce(mapping, g, fn {color_bag, contained}, acc -> Graph.add_edges(acc, parse_edges(color_bag, contained)) end)
+  end
+
+  def color_bag do
+    "shiny gold"
+  end
+end
