c5209ef91612caba4907cd40324d5c8f3f3486c2
Agustin Pomies <agustin@eagerworks.com>
2020-12-18 20:23:55 -0300

    Level 17 - Stage B

--- lib/solvers/17_conway_cubes.ex
+++ lib/solvers/17_conway_cubes.ex
@@ -22,7 +22,7 @@ defmodule ConwayCubes do
         Map.merge(acc, Enum.reduce(
           row,
           %{},
-          fn {column_index, element}, acc -> Map.merge(acc, %{{row_index, column_index, 0} => element}) end
+          fn {column_index, element}, acc -> Map.merge(acc, %{{row_index, column_index} => element}) end
         ))
       end
     )
@@ -36,24 +36,28 @@ defmodule ConwayCubes do
 
   def part_one do
     get_data()
-    |> boot_process(6)
+    |> pad_coordinates(3)
+    |> boot_process(6, 3)
     |> Enum.filter(fn {_coords, state} -> state == :active end)
     |> length()
   end
 
-  def boot_process(board, 0), do: board
-  def boot_process(board, cycles) do
-    execute_cycle(board)
-    |> boot_process(cycles - 1)
+  def boot_process(board, 0, _), do: board
+  def boot_process(board, cycles, dimensions) do
+    case dimensions do
+      3 -> execute_cycle_3d(board)
+      4 -> execute_cycle_4d(board)
+    end
+    |> boot_process(cycles - 1, dimensions)
   end
 
-  def execute_cycle(board) do
-    active_neighbours = Enum.reduce(board, %{}, fn {coords, state}, acc -> if state == :active, do: update_neighbours(acc, neighbours(coords)), else: acc end)
+  def execute_cycle_3d(board) do
+    active_neighbours = Enum.reduce(board, %{}, fn {coords, state}, acc -> if state == :active, do: update_neighbours(acc, neighbours_3d(coords)), else: acc end)
 
     for {coords, neighbours} <- active_neighbours, into: %{}, do: {coords, new_state(board, coords, neighbours)}
   end
 
-  def neighbours({a, b, c}) do
+  def neighbours_3d({a, b, c}) do
     for x <- a-1..a+1, y <- b-1..b+1, z <- c-1..c+1, {x, y, z} != {a, b, c}, do: {x, y, z}
   end
 
@@ -74,6 +78,27 @@ defmodule ConwayCubes do
   end
 
   def part_two do
-    0
+    get_data()
+    |> pad_coordinates(4)
+    |> boot_process(6, 4)
+    |> Enum.filter(fn {_coords, state} -> state == :active end)
+    |> length()
+  end
+
+  def execute_cycle_4d(board) do
+    active_neighbours = Enum.reduce(board, %{}, fn {coords, state}, acc -> if state == :active, do: update_neighbours(acc, neighbours_4d(coords)), else: acc end)
+
+    for {coords, neighbours} <- active_neighbours, into: %{}, do: {coords, new_state(board, coords, neighbours)}
+  end
+
+  def neighbours_4d({a, b, c, d}) do
+    for x <- a-1..a+1, y <- b-1..b+1, z <- c-1..c+1, w <- d-1..d+1, {x, y, z, w} != {a, b, c, d}, do: {x, y, z, w}
+  end
+
+  def pad_coordinates(board, 2), do: board
+  def pad_coordinates(board, dimensions) do
+    extended_board = for {coords, state} <- board, into: %{}, do: {Tuple.append(coords, 0), state}
+
+    pad_coordinates(extended_board, dimensions - 1)
   end
 end
