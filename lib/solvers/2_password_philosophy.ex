4ac8adf7de2746c26c2def360256d4e0439de43d
Agustín Pomies <apomies28@gmail.com>
2022-07-25 21:56:42 -0300

    Refactored Day 2

--- lib/solvers/2_password_philosophy.ex
+++ lib/solvers/2_password_philosophy.ex
@@ -1,94 +1,39 @@
 defmodule PasswordPhilosophy do
+  @no_day 2
+
+  @password_policy_regex ~r"(\d+)-(\d+) (\w): (.+)"
+
+  defp order_attributes([lower_bound, upper_bound, char, password]) do
+    { password, { String.to_integer(lower_bound), String.to_integer(upper_bound), char } }
+  end
+
+  defp parse_password_with_corporate_policy(input_line) do
+    Regex.run(@password_policy_regex, input_line)
+    |> tl()
+    |> order_attributes()
+  end
+
   def get_data do
-    IOModule.get_input(2)
+    IOModule.get_input(@no_day)
+    |> Enum.map(&parse_password_with_corporate_policy(&1))
+  end
+
+  defp meets_old_job_policy?({ password, { min, max, given_letter } = _policy }) do
+    count = String.graphemes(password) |> Enum.count(&(&1 == given_letter))
+
+    Enum.member?(min..max, count)
   end
 
   def part_one do
-    get_data()
-    |> Enum.map(&parse_input_line_1(&1))
-    |> Enum.map(fn map -> [elem(map, 0), elem(map, 1)] end)
-    |> Enum.reduce(0, fn [password, policy], acc ->
-      if check_password_1(password, policy), do: acc + 1, else: acc
-    end)
+    get_data() |> Enum.count(&meets_old_job_policy?(&1))
+  end
+
+  defp meets_official_toboggan_corporate_policy?({ password, { fst_pos, snd_pos, given_letter } = _policy }) do
+    [String.at(password, fst_pos - 1), String.at(password, snd_pos - 1)]
+    |> Enum.count(&(&1 == given_letter)) == 1
   end
 
   def part_two do
-    get_data()
-    |> Enum.map(&parse_input_line_2(&1))
-    |> Enum.map(fn map -> [elem(map, 0), elem(map, 1)] end)
-    |> Enum.reduce(0, fn [password, policy], acc ->
-      if check_password_2(password, policy), do: acc + 1, else: acc
-    end)
-  end
-
-  defp parse_input_line_1(input_line) do
-    regex = ~r"(\d+)-(\d+) (\w): (.+)"
-
-    case Regex.run(regex, input_line) do
-      [_ | tail] -> build_data_1(tail)
-      nil -> IO.puts(input_line)
-    end
-  end
-
-  defp parse_input_line_2(input_line) do
-    regex = ~r"(\d+)-(\d+) (\w): (.+)"
-
-    case Regex.run(regex, input_line) do
-      [_ | tail] -> build_data_2(tail)
-      nil -> IO.puts(input_line)
-    end
-  end
-
-  defp build_data_1([minimum, maximum, character, password]) do
-    policy = {Helper.to_integer(minimum), Helper.to_integer(maximum), character}
-
-    {password, policy}
-  end
-
-  defp check_password_1(password, policy) do
-    minimum = elem(policy, 0)
-    maximum = elem(policy, 1)
-    character = elem(policy, 2)
-
-    password
-    |> String.graphemes()
-    |> Enum.reduce(0, fn char, acc ->
-      if char == character do
-        acc + 1
-      else
-        acc
-      end
-    end)
-    |> inside_range(minimum, maximum)
-  end
-
-  defp inside_range(number, minimum, maximum) do
-    Enum.member?(minimum..maximum, number)
-  end
-
-  defp build_data_2([first_index, second_index, character, password]) do
-    policy = {Helper.to_integer(first_index), Helper.to_integer(second_index), character}
-
-    {password, policy}
-  end
-
-  defp check_password_2(password, policy) do
-    first_index = elem(policy, 0)
-    second_index = elem(policy, 1)
-    character = elem(policy, 2)
-
-    password
-    |> String.graphemes()
-    |> Enum.reduce({false, 1}, fn char, {result, index} ->
-        cond do
-          index == first_index ->
-            {char == character, index + 1}
-          index == second_index ->
-            {Helper.xor(result, char == character), index + 1}
-          true ->
-            {result, index + 1}
-        end
-      end)
-    |> elem(0)
+    get_data() |> Enum.count(&meets_official_toboggan_corporate_policy?(&1))
   end
 end
