16461bc0a793424441d61b745c3f81987b80c6a0
Agustín Pomies <apomies28@gmail.com>
2022-07-27 23:30:25 -0300

    Refactored Day 3

--- lib/solvers/3_toboggan_trajectory.ex
+++ lib/solvers/3_toboggan_trajectory.ex
@@ -1,148 +1,62 @@
 defmodule TobogganTrajectory do
-  def initial_square do
-    {0, 0}
-  end
+  @no_day 3
 
-  def slopes do
-    [{1, 1}, {3, 1}, {5, 1}, {7, 1}, {1, 2}]
-  end
+  @tree "#"
+  @slopes [{1, 1}, {3, 1}, {5, 1}, {7, 1}, {1, 2}]
 
-  def get_data do
-    IOModule.get_input(3) |> parse_board()
-  end
-
-  def part_one do
-    get_data() |> tree_encounters({3, 1})
-  end
-
-  def part_two do
-    board = get_data()
-    tree_encounters = Enum.map(slopes(), &tree_encounters(board, &1))
-    Enum.reduce(tree_encounters, 1, fn x, acc -> x * acc end)
-  end
-
-  def tree_encounters(board, movement) do
-    begin_trajectory(board, initial_square(), movement)
-  end
-
-  def get_input(file_name) do
-    case File.read(file_name) do
-      {:ok, file} -> String.split(file, "\n", trim: true)
-      {:error, reason} -> reason
-    end
-  end
-
-  def parse_board(rows) do
+  defp to_matrix(rows) do
     rows
     |> Enum.map(&String.graphemes(&1))
-    |> Enum.map(fn row ->
-      Enum.map(row, fn symbol -> elem(Map.fetch(tile_type(), symbol), 1) end)
-    end)
+    |> Matrix.from_list()
   end
 
-  def tile_type do
-    %{"." => :square, "#" => :tree}
+  def get_data() do
+    @no_day
+    |> IOModule.get_input()
+    |> to_matrix()
   end
 
-  def char_type do
-    %{square: ".", tree: "#"}
+  defp gen_trajectory(slope, {row_max, col_max}) do
+    slope
+    |> List.wrap()
+    |> Stream.cycle()
+    |> Stream.scan(fn {x, y}, {acc_x, acc_y} -> {acc_x + x, acc_y + y} end)
+    |> Stream.map(fn {x, y} -> {rem(x, col_max), y} end)
+    |> Stream.take_while(fn {_, y} -> y < row_max end)
   end
 
-  def begin_trajectory(board, {x, y}, movement, tree_encounters \\ 0) do
-    initialized_board = move_to_initial_square(board, {x, y})
-    new_tree_encounters = new_tree_encounters(initialized_board, tree_encounters)
-    result = travel_through_board(movement, initialized_board, new_tree_encounters)
-
-    case result do
-      {:halt, result_tree_encounters} ->
-        result_tree_encounters
-
-      {:repeat_board, {new_x, new_y}, result_tree_encounters} ->
-        begin_trajectory(board, {new_x, length(board) - new_y}, movement, result_tree_encounters)
-    end
+  defp tree_encounters(trajectory, board) do
+    trajectory
+    |> Stream.map(fn {i, j} -> if board[j][i] == @tree, do: 1, else: 0 end)
+    |> Stream.scan(&(&1 + &2))
+    |> Stream.take(-1)
+    |> Enum.to_list()
+    |> hd()
   end
 
-  def move_to_initial_square(board, {x, y}) do
-    board
-    |> Enum.drop(y)
-    |> Enum.map(&Enum.drop(&1, x))
+  defp do_slide(slide, board) do
+    limits = Matrix.dimensions(board)
+
+    slide
+    |> gen_trajectory(limits)
+    |> tree_encounters(board)
   end
 
-  def travel_through_board(movement, [_ | [_ | _]] = board, tree_encounters) do
-    case apply_movement(board, movement) do
-      {:travel, new_board} ->
-        travel_through_board(movement, new_board, new_tree_encounters(new_board, tree_encounters))
+  def part_one() do
+    board = get_data()
+    limits = Matrix.dimensions(board)
 
-      {:change_board, initial_square} ->
-        {:repeat_board, initial_square, tree_encounters}
-
-      {:halt, final_board} ->
-        {:halt, new_tree_encounters(final_board, tree_encounters)}
-    end
+    @slopes
+    |> Enum.at(1)
+    |> do_slide(board)
   end
 
-  def travel_through_board(_, [_ | _], tree_encounters) do
-    {:halt, tree_encounters}
-  end
+  def part_two() do
+    board = get_data()
+    limits = Matrix.dimensions(board)
 
-  def apply_movement(board, movement = {x, y}) do
-    cond do
-      length(board) > y && length(hd(board)) > x -> travel(board, movement)
-      length(hd(board)) <= x -> change_board(board, movement)
-      length(board) <= y -> check_final_tile(board, movement)
-    end
-  end
-
-  def travel(board, {x, y}) do
-    new_board =
-      board
-      |> Enum.drop(y)
-      |> Enum.map(&Enum.drop(&1, x))
-
-    {:travel, new_board}
-  end
-
-  def change_board(board = [row | _], {x, y}) do
-    initial_square = {x - length(row), length(board) - y}
-
-    {:change_board, initial_square}
-  end
-
-  def check_final_tile(board, {x, _}) do
-    final_row =
-      board
-      |> List.last()
-      |> Enum.drop(x)
-
-    {:halt, [final_row]}
-  end
-
-  def new_tree_encounters(board, tree_encounters \\ 0) do
-    if tree_encountered?(board), do: tree_encounters + 1, else: tree_encounters
-  end
-
-  def tree_encountered?(board) do
-    tile =
-      board
-      |> hd()
-      |> hd()
-
-    tile == :tree
-  end
-
-  # Debug Tools
-
-  def print_board(board) do
-    board
-    |> board_to_chars()
-    |> IO.puts()
-  end
-
-  def board_to_chars(board) do
-    Enum.join(Enum.map(board, &row_to_chars(&1)), "\n")
-  end
-
-  def row_to_chars(row) do
-    Enum.join(Enum.map(row, &elem(Map.fetch(char_type(), &1), 1)))
+    @slopes
+    |> Enum.map(&do_slide(&1, board))
+    |> Enum.reduce(&*/2)
   end
 end
